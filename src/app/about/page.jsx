0e32a1b02a001c6d66898e0ed3ee51b291e06c93
Tawhidul Islam <138350536+tawhid3482@users.noreply.github.com>
2025-02-21 20:40:25 +0600

    link

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,24 @@
+"use client"
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Page = () => {
+
+  const router = useRouter()
+  const isLogin = false
+  const handleNavigation = ()=>{
+    if(isLogin){
+      router.push('/about/address')
+    }else{
+      router.push('/')
+    }
+  }
   return (
     <div>
       <p className='text-xl font-bold text-purple-400'>about</p>
+      {/* <Link href={'/about/address'}>Address</Link> */}
+      <button type='button' onClick={handleNavigation}>Address page</button>
     </div>
   );
 };
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
 
 const Navbar = () => {
     return (
         <div className="">
             <nav className="flex justify-center items-center h-16 border border-green-600">
                 <ul className="flex justify-between w-1/2">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Service</li>
-                    <li>blog</li>
+                    <Link href={'/'}>Home</Link>
+                    <Link href={'/services'}>services</Link>
+                    <Link href={'/about'}>about</Link>
+                    {/* <Link to>Home</Link> */}
+                   
                 </ul>
             </nav>
         </div>
